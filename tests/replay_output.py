d9f92b63a0d620189eab9ce20008dc3d24341938
LMicol <lucas.micol.p@gmail.com>
2024-08-25 17:36:25 -0300

    testing another thing

--- tests/replay_output.py
+++ tests/replay_output.py
@@ -4,6 +4,7 @@
  
 import soundcard as sc
 import soundfile as sf
+import pickle as pkl
 import time
 
 # get a list of all speakers:
@@ -14,7 +15,7 @@ default_speaker = sc.default_speaker()
 # get a list of all microphones:v
 mics = sc.all_microphones(include_loopback=True)
 # get the current default microphone on your system:
-default_mic = mics[3]
+default_mic = mics[0]
 
 for i in range(len(mics)):
     try:
@@ -25,8 +26,11 @@ for i in range(len(mics)):
 with default_mic.recorder(samplerate=148000) as mic, default_speaker.player(samplerate=148000) as sp:
     print("Recording...")
     data = mic.record(numframes=1000000)
-    print("Done...Stop your sound so you can hear playback")
-    time.sleep(5)
-    sp.play(data)
+    print(data.shape)
+    with open("audio.pkl", 'wb') as f:
+        pkl.dump(data, f)
+    # print("Done...Stop your sound so you can hear playback")
+    # time.sleep(2)
+    # sp.play(data)
 
 sf.write("filename.wav", data, 148000)
